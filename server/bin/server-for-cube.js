13d39065f2a684d7eea99a05eef4238e4fc270b5
Nikita Teremovskiy <teremovskiin@gmail.com>
2020-12-28 23:26:15 +0300

    fix: change server-for-web.js

--- server/bin/server-for-cube.js
+++ server/bin/server-for-cube.js
@@ -1,7 +1,23 @@
 import makeServer from '../index.js';
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-let port = process.env.PORT;
-if (port == null || port === "") {
-  port = 4000;
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  makeServer(PORT);
 }
-makeServer(port);
